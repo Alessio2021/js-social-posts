d025398cf304e5ea67cebe5c4f2a92f3fc1c2b62
Alessio2021 <alessiobut1911@gmail.com>
2021-12-10 16:59:43 +0100

    FUNCTION

--- js/script.js
+++ js/script.js
@@ -32,59 +32,68 @@ const users = [
         likes: 98
     },
 ]
+const cardPrint = card(users)
 
-for (let i = 0; i < users.length; i++) {
-    const newPost = users[i];
-
-    const element = 
-        `
-        <div class="post">
-        <div class="post__header">
-            <div class="post-meta">                    
-                <div class="post-meta__icon">
-                    <img class="profile-pic" src="https://unsplash.it/300/300?${newPost.imageUser}" alt="${newPost.guest}">                    
-                </div>
-                <div class="post-meta__data">
-                    <div class="post-meta__author">${newPost.guest}</div>
-                    <div class="post-meta__time">${newPost.date}</div>
-                </div>                    
-            </div>
-        </div>
-        <div class="post__text">${newPost.text}</div>
-        <div class="post__image">
-            <img src="https://unsplash.it/600/300?${newPost.image}" alt="">
-        </div>
-        <div class="post__footer">
-            <div class="likes js-likes">
-                <div class="likes__cta">
-                    <a class="like-button  js-like-button" data-postid="1">
-                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                        <span class="like-button__label">Mi Piace</span>
-                    </a>
-                </div>
-                <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">${newPost.likes}</b> persone
-                </div>
-            </div> 
-        </div>            
-    </div>
-    `;
-
-    container.innerHTML += element;
-}
 
 const button = document.querySelectorAll('.js-like-button');
 let like = document.querySelectorAll('.js-likes-counter');
-
 for (let i = 0; i < button.length; i++) {
+
     button[i].addEventListener('click', function (){
         // console.log(button[i]);
         button[i].classList.toggle('like-button--liked');
-
+        
         if (button[i].classList.contains('like-button--liked') == true) {
-            like.innerHTML = users[i].likes += 1;
+            like[i].innerHTML = users[i].likes += 1;
         } else {
-            like.innerHTML = users[i].likes -= 1;
+            like[i].innerHTML = users[i].likes -= 1;
         }
     });
+};
+
+
+
+
+// FUNCTION
+function card(array) {
+    for (let i = 0; i < array.length; i++) {
+        const newPost = array[i];
+    
+        const element = 
+            `
+            <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="https://unsplash.it/300/300?${newPost.imageUser}" alt="${newPost.guest}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${newPost.guest}</div>
+                        <div class="post-meta__time">${newPost.date}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${newPost.text}</div>
+            <div class="post__image">
+                <img src="https://unsplash.it/600/300?${newPost.image}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${newPost.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+        `;
+    
+        container.innerHTML += element;
+    }
+    
 }
