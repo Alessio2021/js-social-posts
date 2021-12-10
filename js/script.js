223cee1c113ac3c773ad8407d4c884a1e3ef6ba1
Alessio2021 <alessiobut1911@gmail.com>
2021-12-10 15:57:14 +0100

    array

---
+++ js/script.js
@@ -0,0 +1,30 @@
+const users = [
+    {
+        guest: 'Phil Mangione',
+        imageUser: 'image=15',
+        date: '4 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut.Distinctio minima iusto.Ad ad maiores et sint voluptate recusandae architecto.Et nihil ullam aut alias.',
+        image: 'image=171'
+    },
+    {
+        guest: 'Jhonny Bravo',
+        imageUser: 'image=16',
+        date: '5 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut.Distinctio minima iusto.Ad ad maiores et sint voluptate recusandae architecto.Et nihil ullam aut alias.',
+        image: 'image=172'
+    },
+    {
+        guest: 'James Bond',
+        imageUser: 'image=17',
+        date: '6 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut.Distinctio minima iusto.Ad ad maiores et sint voluptate recusandae architecto.Et nihil ullam aut alias.',
+        image: 'image=173'
+    },
+    {
+        guest: 'Tony Stark',
+        imageUser: 'image=18',
+        date: '7 mesi fa',
+        text: 'Placeat libero ipsa nobis ipsum quibusdam quas harum ut.Distinctio minima iusto.Ad ad maiores et sint voluptate recusandae architecto.Et nihil ullam aut alias.',
+        image: 'image=174'
+    },
+]
